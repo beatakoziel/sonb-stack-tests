ed46824519b52a2646d5f170e055bbc73dc72cc5
Beata Kozieł <beatainez.k@gmail.com>
2021-11-29 22:54:33 +0100

    reformat

--- src/test/java/MyStackTest.java
+++ src/test/java/MyStackTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
